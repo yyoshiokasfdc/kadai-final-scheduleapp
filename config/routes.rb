06440d699c1ca5887f1cca0249e5a41426bceada
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-18 11:39:54 +0000

    7.2 done, next:20.7.3 UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'schedules#index'    
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
   resources :schedules
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_050057) do
+ActiveRecord::Schema.define(version: 2021_12_18_113500) do
 
   create_table "schedules", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
