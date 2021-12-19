d426e22b8a5165d6e40e0954efd975098428e500
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-19 10:06:03 +0000

    second submission

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,7 @@ class Schedule < ApplicationRecord
     belongs_to :user
     
     validates :content, presence: true, length: { maximum: 255 }
-    validates :start_time, :end_time, presence: true
+    validates :end_time, presence: true
+    validates :start_time, presence: true, numericality: { less_than: :end_time}
+    
 end
